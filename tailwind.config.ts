5761aed91e3db644acd22ae0f9c2f489e4feca92
Emerson Shatouhy <emersont2003@gmail.com>
2024-01-23 15:01:26 +0000

    First Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,26 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'background': '#89A1C2',
+      'button-background': '#3F5938',
+      'button-background-hover': '#4F6A48',
+      'button-text': '#DFCEAB',
+      'primary-text': '#0C4603',
+      'secondary-text': '#1A1423',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-inter)'],
       },
     },
   },
